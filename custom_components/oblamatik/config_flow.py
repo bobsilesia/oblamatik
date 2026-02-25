59d73d51e89af1ffcafa60e6baeb1a557186e3f2
Robert Psiurski <robertpsiurski@MacBook-Pro-Robert.local>
2026-02-25 02:51:25 +0100

    Fix mypy error: Add type hint for errors dict, bump v2.1.11

--- custom_components/oblamatik/config_flow.py
+++ custom_components/oblamatik/config_flow.py
@@ -16,7 +16,7 @@ class ConfigFlow(config_entries.ConfigFlow):
     domain = DOMAIN
 
     async def async_step_user(self, user_input: dict[str, Any] | None = None) -> Any:
-        errors = {}
+        errors: dict[str, str] = {}
 
         if user_input is not None:
             host = user_input[CONF_HOST]
